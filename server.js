1ab90ff5909a98818d2a25a4c741adfce409427b
Luke Weber <lukeweber12@gmail.com>
2021-08-25 13:42:30 +0000

    try to get a post to work

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var fs = require("fs");
 
 app.get('/HealthCheck', function (req, res) {
    console.log("Healthy");
-   res.json({ health: true });
+   res.status(200);
+})
+
+app.post('/Suggestion', function (req, res) {
+   console.log("posted");
+   res.status(200);
 })
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+  
 })
