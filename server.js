ef50a5f05df1629de065ff1c1c0d55fe286c2f82
Luke Weber <lukeweber12@gmail.com>
2021-08-28 18:52:19 +0000

    Get Get working

--- server.js
+++ server.js
@@ -3,25 +3,38 @@ var app = express();
 const cors = require("cors");
 const { json } = require('express');
 const bodyParser  = require('body-parser');
+const MySqlConnection = require('./MySqlConnection');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json())
+let connection = MySqlConnection.createConnection();
+connection.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+ });
+
 app.get('/HealthCheck', function (req, res) {
    console.log("Healthy");
+   
    res.status(200);
    res.send();
 })
 
 app.post('/Suggest', function (req, res) {
    console.log("posted");
+   connection.query("Insert Into Suggestions (Suggestion) values (?)",req.body , function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
    res.status(200);
    res.send();
+   
 })
 
 app.put("/Update", function(req, res){
    
       data = req.body;
-   console.log("Update"+ data);
+   console.log("Update"+ Json.stringify(data));
    res.status(200);
    res.send();
 })
@@ -36,15 +49,23 @@ app.delete("/Delete", function(req, res){
 
 app.get('/Suggestions', function(req, res){
    console.log("get Suggestions 3")
-   res.send([
-               {ID:1,Title:"Shorter"}, 
-               {ID:2,Title:"DUH"}
-            ]
-   );
+   connection.query("Select Id, Suggestion from Suggestions" , function (err, result, fields) {
+      if (err) {
+         res.status(500)
+         res.send()
+         throw err;
+      }
+      console.log(JSON.stringify(result));
+      res.send(JSON.stringify(result))
+    
+    });
 })
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
   
 })
+
